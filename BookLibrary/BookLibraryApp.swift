9f058a6897584e7f583eb3223d8a963d6fb94cde
Mustafa Emre Telli <emretelli00@hotmail.com>
2023-12-04 14:11:44 +0300

    Initial Commit

---
+++ BookLibrary/BookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLibraryApp.swift
+//  BookLibrary
+//
+//  Created by Emre Telli on 4.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
